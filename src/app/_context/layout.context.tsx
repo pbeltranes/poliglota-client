55ad0915418a4972d4508ae69d96c078c1df0336
paul <p.beltranes@gmail.com>
2024-07-15 17:17:07 -0400

    🐛 feat: Add lazy loading for images and fix localStorage bug

--- src/app/_context/layout.context.tsx
+++ src/app/_context/layout.context.tsx
@@ -10,7 +10,7 @@ import {
   useEffect,
 } from "react";
 
-interface layoutInterface {
+interface LayoutInterface {
   theme: string;
   lang: string;
   locale: string;
@@ -18,8 +18,8 @@ interface layoutInterface {
 }
 
 type LayoutContextType = [
-  layoutInterface,
-  Dispatch<SetStateAction<layoutInterface>>,
+  LayoutInterface,
+  Dispatch<SetStateAction<LayoutInterface>>,
   () => void,
   (langLocal: string) => void
 ];
@@ -27,48 +27,50 @@ type LayoutContextType = [
 const Layout = createContext<LayoutContextType | undefined>(undefined);
 
 export function LayoutProvider({ children }: { children: ReactNode }) {
-  const [layout, setLayout] = useState<layoutInterface>({
-    theme: localStorage.getItem("theme") || "light",
-    lang: localStorage.getItem("lang") || "es",
-    locale: localStorage.getItem("locale") || "cl",
-    path: {},
+  
+  const [layout, setLayout] = useState<LayoutInterface>(() => {
+    if (typeof window !== "undefined") {
+      return {
+        theme: localStorage.getItem("theme") || "light",
+        lang: localStorage.getItem("lang") || "es",
+        locale: localStorage.getItem("locale") || "cl",
+        path: {},
+      };
+    }
+    return {
+      theme: "light",
+      lang: "es",
+      locale: "cl",
+      path: {},
+    };
   });
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const savedTheme = localStorage.getItem("theme");
-      const savedLang = localStorage.getItem("lang");
-      const savedLocale = localStorage.getItem("locale");
-
-      if (savedTheme && savedLang && savedLocale) {
-        setLayout((prev) => ({
-          ...prev,
-          theme: savedTheme,
-          lang: savedLang,
-          locale: savedLocale,
-        }));
-      }
-      fetchData();
-    }
-  }, [layout.theme, layout.lang, layout.locale]);
+    fetchData();
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem("theme", layout.theme);
-    localStorage.setItem("lang", layout.lang);
-    localStorage.setItem("locale", layout.locale);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", layout.theme);
+      localStorage.setItem("lang", layout.lang);
+      localStorage.setItem("locale", layout.locale);
+    }
   }, [layout.theme, layout.lang, layout.locale]);
 
   const fetchData = async () => {
     try {
       const response = await fetch("/api/config");
       const data = await response.json();
-      setLayout((prev) => {
-        return { ...prev, path: data.path };
-      });
+      //todo: validacion si lang y locale no se encuentra dentro del data, poner por defecto es/cl
+      setLayout((prev) => ({
+        ...prev,
+        path: data.path,
+      }));
     } catch (error) {
       console.error("Error fetching layout data:", error);
     }
   };
+
   const toggleTheme = () => {
     setLayout({
       ...layout,
